45eafb44b051bd77feddfe92132f3d886347ed24
RudyCortedano <rudycortedano028@gmail.com>
2023-11-05 20:41:13 -0600

    5.sintaxis

--- src/models/Actors.js
+++ src/models/Actors.js
@@ -3,11 +3,11 @@ const sequelize = require('../utils/connection');
    // En Mayúsculas y singular      // en minúsculas y singular
 const Actors = sequelize.define('actors', {
     // Definimos las columnas aquí
-    firstname: {
+    firstName: {
         type: DataTypes.STRING(50),
         allowNull: false
     },
-    lastname: {
+    lastName: {
         type: DataTypes.STRING(50),
         allowNull: false
     },
