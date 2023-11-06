f62093cda51cf6d4a9e0ef4f5238b9f3d96070c5
RudyCortedano <rudycortedano028@gmail.com>
2023-11-05 22:34:03 -0600

    solucionando error de columnas

--- src/models/Directors.js
+++ src/models/Directors.js
@@ -3,6 +3,7 @@ const sequelize = require('../utils/connection');
    // En Mayúsculas y singular      // en minúsculas y singular
 const Directors = sequelize.define('directors', {
     // Definimos las columnas aquí
+    //  solucionando problema de columnas commit 6
     firstName: {
         type: DataTypes.STRING(50),
         allowNull: false
